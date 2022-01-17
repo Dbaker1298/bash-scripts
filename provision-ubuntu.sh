6f1192fbc125390a0a9a476bd9df0da715fbf3e0
Dbaker1298 <dpbaker1298@gmail.com>
2022-01-17 14:45:44 -0500

    Adding ubuntu provision script

---
+++ provision-ubuntu.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e
+set -x
+
+# Add my user
+useradd  david
+echo "Remember to set the password for david"
+echo "david  ALL=(ALL)  NOPASSWD: ALL" >> /etc/sudoers.d/david
+echo "Pl3a8s3!}chan83"
+
+# Install docker 
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+sleep 30
+usermod -aG docker david
+docker run hello-world
+echo "Time to generate or copy SSH keys"
+sleep 3 
+# Install kubectl
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+sleep 5
+# Install HELM
+curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm
+sleep 5
+# Install k3d
+curl -s https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+sleep 3
+# Install k9s
+curl -sS https://webinstall.dev/k9s | bash
+sleep 3
+# Install kubeadm
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+EOF
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+sudo sysctl --system
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+sleep 5
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm
+sleep 5
+
+# Install RKE
+curl -s https://api.github.com/repos/rancher/rke/releases/latest | grep download_url | grep amd64 | cut -d '"' -f 4 | wget -qi -
+chmod +x rke_linux-amd64
+sudo mv rke_linux-amd64 /usr/local/bin/rke
+rke --version
+
